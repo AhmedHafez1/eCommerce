0b664cac1d43f5d2a045ee283322f4902ff2810b
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-05-01 18:39:53 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "187 Products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return $"Product with id {id}";
+        }
+    }
+}
