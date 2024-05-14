ee505052b1f92b361205509fbb699902a0d0d415
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-05-14 19:21:56 +0300

    App User

---
+++ Core/Entities/Identity/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entities.Identity
+{
+    public class Address
+    {
+        public string Building { get; set; } = null!;
+        public string Street { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string? PostalCode { get; set; }
+        public string Country { get; set; } = null!;
+        public AppUser AppUser { get; set; } = null!;
+        public string AppUserId { get; set; } = null!;
+    }
+}
