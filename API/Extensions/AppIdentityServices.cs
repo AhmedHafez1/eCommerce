259d4d78df59030e49e0db4079bd5ffa21a21b1f
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-05-17 06:25:44 +0300

    Token Service

--- API/Extensions/AppIdentityServices.cs
+++ API/Extensions/AppIdentityServices.cs
@@ -3,6 +3,8 @@ using Infrastructure.Data.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace API.Extensions
 {
@@ -21,7 +23,17 @@ namespace API.Extensions
             }).AddEntityFrameworkStores<AppIdentityContext>()
             .AddSignInManager<SignInManager<AppUser>>();
 
-            services.AddAuthentication();
+            services.AddAuthentication()
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"]!)),
+                        ValidateIssuer = true,
+                        ValidIssuer = config["Token:Issuer"]
+                    };
+                });
             services.AddAuthorization();
 
             return services;
